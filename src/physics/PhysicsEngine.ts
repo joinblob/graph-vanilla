4abea75b5ffbfa77279f40c21bb3ab056791a947
Lakshya Gupta <laytoder@gmail.com>
2022-07-02 17:28:42 +0530

    N2 initial commit

--- src/physics/PhysicsEngine.ts
+++ src/physics/PhysicsEngine.ts
@@ -1,21 +1,3 @@
-import LinkedList from "./helpers/LinkedList";
-import PointObject from "./pointComponent";
-
-class PhysicsEngine {
-  private static stable: LinkedList<PointObject> =
-    new LinkedList<PointObject>();
-  private static unstable: LinkedList<PointObject> =
-    new LinkedList<PointObject>();
-
-  public static declareCurrentUnstable(): void {
-    const deleted: PointObject = PhysicsEngine.stable.delete();
-    PhysicsEngine.unstable.insertBefore(deleted);
-  }
-
-  public static declareCurrentStable(): void {
-    const deleted: PointObject = PhysicsEngine.unstable.delete();
-    PhysicsEngine.stable.add(deleted);
-  }
-}
+class PhysicsEngine {}
 
 export default PhysicsEngine;
